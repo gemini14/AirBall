385bf480e9f6efee5cb8be38e5f4b49d88787444
Tarik Sturm-Dahal <devnull@localhost>
2010-08-31 06:47:37 -0400

    -missing tile and smoke textures (appeared corrupted, at least when I tried to get them--will add them in on next push) -inserted VentParticle .h and .cpp files again, included in Level.h, will integrate with new level code from rev. 37 -removed unnecessary font files -removed unnecessary image and material -updated media files in project resource filter (was missing a few)

--- Proj_2/Level.h
+++ Proj_2/Level.h
@@ -14,6 +14,7 @@
 #include <vector>
 
 #include "PhysicsManager.h"
+#include "VentParticles.h"
 
 
 namespace Tuatara
---
+++ Proj_2/VentParticles.h
@@ -0,0 +1,25 @@
+#ifndef VENTPARTICLES_H
+#define VENTPARTICLES_H
+
+#include <irrlicht.h>
+#include <string>
+
+#include "EnumsConstants.h"
+#include "Game.h"
+
+namespace Tuatara
+{
+	class VentParticles
+	{
+		irr::scene::IParticleSystemSceneNode* particleSystem;
+
+	public:
+		VentParticles(void);
+		~VentParticles(void);
+
+		void Start (Game& game, irr::core::vector3df position, 
+			irr::core::vector3df normalDirection, irr::f32 distance);
+	};
+}
+
+#endif
