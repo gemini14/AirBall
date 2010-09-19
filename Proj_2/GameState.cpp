ea09fdb845970f726b0c7b77f28fa2961772f5aa
Dylan <devnull@localhost>
2010-09-19 10:50:17 -0700

    - Reset first level (I had changed it to start with level 3 for debug purposes and forgot to put it back). - Third level is a test level, but I made it have an exit in keeping with the design of a game, at least slightly... :)

--- Proj_2/GameState.cpp
+++ Proj_2/GameState.cpp
@@ -4,7 +4,7 @@
 namespace Tuatara
 {
 
-	GameState::GameState() : gameEventRcvr( new GameStateEventReceiver ), currentLevel( 3 )
+	GameState::GameState() : gameEventRcvr( new GameStateEventReceiver ), currentLevel( 0 )
 	{
 		using namespace std;
 
